242857a4b8654cfac4925415d2ac5e47a91fd88f
Jonathan Tapia <tapiaxj@gmail.com>
2018-04-02 23:40:21 -0700

    Finished scanner

--- Scanner/scannerFinal.cpp
+++ Scanner/scannerFinal.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <iterator>
 
 #include <unordered_map>
 
@@ -32,77 +34,85 @@ bool periodToken(string s)
 bool wordToken(string s) 
 {
 	char c;
+	char prevc;
+	bool stuck = false;
 
 	string state = "q0";
-	/* 
-		Possible states:
-		q0 (c == 'n')
-		v (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-		c (c == 'c')
-		s (c == 's')
-		cs_h (c == 'h')
-		t (c == 't')
-		t_s (c == 's')
-		spc (c == 'b' || c == 'g' || c == 'h' || c == 'k' || c == 'm' || c == 'n' || c == 'p' || c == 'r')
-		spc_y (c == 'y')
-		nsc (c == 'd' || c == 'j' || c == 'w' || c == 'y' || c == 'z')
-	*/
 
+	// Very large combination of states.
+	string every[] = { "q0", "v", "s", "t", "spc", "nsc", "cs_h", "t_s", "spc_y" };
+	// Common combination of states.
+	string q0_v[] = { "q0", "v" };
+	// All legitimate characters for this scanner.
+	string vowels = "aeiouAEIOU";
+	string v_n = "nN";
+	string s_ = "sS";
+	string c_ = "cC";
+	string cs_h = "hH";
+	string t_ = "tT";
+	string t_s = "sS";
+	string spc_cons = "bghkmnprBGHKMNPR";
+	string spc_cons_y = "yY";
+	string nonspc_cons = "djwyzDJWYZ";
+
+	//
 	for (int i = 0; i < s.length(); i++) {
 		c = s[i];
-		cout << c << ": ";
+
+		//Print the current character.
+		//cout << "\t" << c << ": ";
 
 		// Trs({q0, v, s, t, spc, nsc, cs_h, t_s, spc_y}, {'a', 'e', 'i', 'o', 'u'}) = v
-		if ((state == "q0" || state == "v" || state == "s" || state == "t" || state == "spc" || state == "nsc" || state == "cs_h" || state == "t_s" || state == "spc_y")
-			&& (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')) 
-		{
+		if ((find(begin(every), end(every), state) != end(every)) && vowels.find(c) != string::npos) {
 			state = "v";
 		}
 		// Trs({v}, {'n'}) = q0
-		else if (state == "v" && c == 'n') {
+		else if ((state == "v") && v_n.find(c) != string::npos) {
 			state = "q0";
 		}
 		// Trs({q0, v}, {'s'}) = s
-		else if ((state == "q0" || state == "v") && c == 's') {
+		else if (find(begin(q0_v), end(q0_v), state) != end(q0_v) && s_.find(c) != string::npos) {
 			state = "s";
 		}
 		// Trs({q0, v}, {'c'}) = c
-		else if ((state == "q0" || state == "v") && c == 'c') {
+		else if (find(begin(q0_v), end(q0_v), state) != end(q0_v) && c_.find(c) != string::npos) {
 			state = "c";
 		}
 		// Trs({q0, v}, {'t'}) = t
-		else if ((state == "q0" || state == "v") && (c == 't')) {
+		else if (find(begin(q0_v), end(q0_v), state) != end(q0_v) && t_.find(c) != string::npos) {
 			state = "t";
 		}
-
-		else if ((state == "q0" || state == "v") && (c == 'd' || c == 'j' || c == 'w' || c == 'y' || c == 'z')) {
+		// Trs({q0, v}, {'d', 'j', 'w', 'y', 'z'}) = nsc
+		else if (find(begin(q0_v), end(q0_v), state) != end(q0_v) && nonspc_cons.find(c) != string::npos) {
 			state = "nsc";
 		}
-
-		else if ((state == "q0" || state == "v") && (c == 'b' || c == 'g' || c == 'h' || c == 'k' || c == 'm' || c == 'n' || c == 'p' || c == 'r')) {
+		// Trs({q0, v}, {'b', 'g', 'h', 'k', 'm', 'n', 'p', 'r'}) = spc
+		else if (find(begin(q0_v), end(q0_v), state) != end(q0_v) && spc_cons.find(c) != string::npos) {
 			state = "spc";
 		}
-
-		else if (state == "spc" && c == 'y') {
+		// Trs({spc}, {'y'}) = spc_y
+		else if (state == "spc" && spc_cons_y.find(c) != string::npos) {
 			state = "spc_y";
 		}
-
-		else if ((state == "c" || state == "s") && c == 'h') {
+		// Trs({c}, {'s'}) = cs_h
+		else if ((state == "c" || state == "s") && cs_h.find(c) != string::npos) {
 			state = "cs_h";
 		}
-
-		else if (state == "t" && c == 's') {
+		// Trs({t}, {'s'}) = t_s
+		else if (state == "t" && t_s.find(c) != string::npos) {
 			state = "t_s";
 		}
-
+		//Error
 		else {
-			cout << "\nStuck in state " << state;
+			//cout << "\n\tStuck in state " << state << endl;
+			stuck = true;
 			break;
 		}
 
-		cout << state << endl;
+		//Print the current state.
+		//cout << state << endl;
 	}
-	if (state == "q0" || state == "v") {
+	if (!stuck && (state == "q0" || state == "v")) {
 		return true;
 	}
 	return false;
@@ -113,6 +123,7 @@ bool wordToken(string s)
 ifstream fin;
 enum tokenType {ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, 
 WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR, EOFM};
+
 unordered_map<string, tokenType> reservedWords;
 
 string tokenNames[30] = 
@@ -151,12 +162,15 @@ void setUpReservedWordsTable()
 
 // Scaner processes only one word at a time
 // Done by: Jonathan
-void scanner(tokenType& type, string& word)
+int scanner(tokenType& type, string& word)
 {
 	fin >> word;
 
-	if (wordToken(word)) {
-		auto pair = reservedWords.find(word);
+	if(word == "eofm"){
+		exit(0);
+	}
+	else if (wordToken(word)) {
+		unordered_map<string,tokenType>::const_iterator pair = reservedWords.find(word);
 		if (pair == reservedWords.end()) {
 			//Not found
 
