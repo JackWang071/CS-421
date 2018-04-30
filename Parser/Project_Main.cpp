7180d0cf1d536528ad4778031660df73bf90207f
JackWang071 <35980971+JackWang071@users.noreply.github.com>
2018-04-29 22:17:05 -0700

    Add files via upload

---
+++ Parser/Project_Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+#include "parser.h"
+
+int main() {
+	Parser test;
+	test.startup();
+	return 0;
+}
---
+++ Parser/scanner.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef scanner_H
+#define scanner_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <unordered_map>
+
+using namespace std;
+
+enum tokentype {
+	ERROR, WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS,
+	WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR, EOFM
+};
+
+class Scanner {
+private:
+	ifstream fin;
+	unordered_map<string, tokentype> reservedWords;
+	string tokenNames[30] =
+	{
+		"ERROR", "WORD1", "WORD2", "PERIOD", "VERB", "VERBNEG", "VERBPAST", "VERBPASTNEG", "IS",
+		"WAS", "OBJECT", "SUBJECT", "DESTINATION", "PRONOUN", "CONNECTOR", "EOFM"
+	};
+public:
+	Scanner(string filename);
+	int scanner(tokentype& type, string& word);
+	void setUpReservedWordsTable();
+	bool periodToken(string s);
+	bool wordToken(string s);
+};
+
+#endif // !scanner_H
